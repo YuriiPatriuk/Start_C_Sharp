622982fe3065bbdae8defd701795bdd88abfc588
Annonymous <dev@rivne.itstep.org>
2021-03-01 20:49:15 +0200

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,65 @@
+﻿//OOP only!!
+
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static decimal plus(decimal a, decimal b)
+        {
+            return a + b;
+        }
+        static decimal mult(decimal a, decimal b)
+        {
+            return a * b;
+        }
+        static void calc()
+        {
+            char operation = 'w';
+            decimal n1 = 0;
+            decimal n2 = 0;
+            do{ 
+            Console.WriteLine("[i]. Input numbers");
+            Console.WriteLine("[+]. Plus numbers");
+            Console.WriteLine("[*]. Mult numbers");
+            Console.WriteLine("[q]. Quit");
+            Console.WriteLine("Enter operation: ");
+            operation = char.Parse(Console.ReadLine());
+                switch (operation)
+                {
+                    case 'i':
+                    case 'I':
+                        try
+                        {
+                            Console.WriteLine("Enter first number: ");
+                            n1 = Convert.ToDecimal(Console.ReadLine());
+                            Console.WriteLine("Enter second number: ");
+                            n2 = decimal.Parse(Console.ReadLine());
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"Error: {ex.Message}");
+                        }
+                        break;
+                    case '+':
+                        Console.WriteLine($"{n1}+{n2}={plus(n1, n2)}"); //інтерполяційний рядок
+                        break; //return, goto
+                    case '*':
+                        Console.WriteLine("{0}*{1}={2}", n1, n2, mult(n1, n2));
+                        break;
+                    default:
+                        Console.WriteLine("Error choose!!");
+                        break;
+                }
+                Console.ReadKey();
+                Console.Clear();
+            } while (char.ToLower(operation)!= 'q');
+
+        }
+        static void Main()
+        {
+            calc();
+        }
+    }
+}
---
+++ class intro/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+namespace class_intro
+{
+    //class Number
+    //{
+    //    private int number;
+    //    public Number(int num)
+    //    {
+    //        this.number = num;
+    //    }
+    //    public Number() : this(1)
+    //    { }
+    //    public void Inc()
+    //    {
+    //        ++number;
+    //    }
+    //    public override string ToString()
+    //    {
+    //        return $"Number: {number}";
+    //    }
+    //}
+
+    class ATM 
+    {
+        private string id;
+        private string password;
+        private double balance;
+        public ATM(string id, string pass, double balance)
+        {
+            this.id = id;
+            this.password = pass;
+            this.balance = balance;
+        }
+        public string getPassword()
+        {
+            return password;
+        }
+        public string getID()
+        {
+            return id;
+        }
+        public void ShowBalance()
+        {
+            Console.WriteLine($"Balance: {balance}");
+        }
+        public void AddMoney(double addedMoney)
+        {
+            balance += addedMoney;
+        }
+        public void Withdraw(double withdrawMoney)
+        {
+            if (balance < withdrawMoney)
+                balance = 0;
+            else
+                balance -= withdrawMoney;
+        }
+        public void menu()
+        {
+            Console.WriteLine("1. Show balance");
+            Console.WriteLine("2. Add money");
+            Console.WriteLine("3. Withdraw");
+            Console.WriteLine("4. Exit");
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Number num = new Number(12);
+            //Console.WriteLine(num);
+            int choose = 0;
+            string pass, id;
+
+            ATM atm = new ATM("1002", "6000", 3500);
+            Console.Write("Enter id: ");
+            id = Console.ReadLine();
+            Console.Write("Enter password: ");
+            pass = Console.ReadLine();
+            if (pass == atm.getPassword() && id == atm.getID())
+            {
+                while (choose != 4)
+                {
+                    atm.menu();
+                    choose = int.Parse(Console.ReadLine());
+                    double money;
+                    switch (choose)
+                    {
+                        case 1:
+                            atm.ShowBalance();
+                            break;
+                        case 2:
+                            Console.Write("Enter money: ");
+                            money = double.Parse(Console.ReadLine());
+                            atm.AddMoney(money);
+                            break;
+                        case 3:
+                            Console.Write("Enter money: ");
+                            money = double.Parse(Console.ReadLine());
+                            atm.Withdraw(money);
+                            break;
+                        case 4:
+                            Console.WriteLine("Goodbye!!");
+                            break;
+                        default:
+                            Console.WriteLine("Error, choose correct position");
+                            break;
+                    }
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error id or password!!");            }
+
+        }
+    }
+}
+
+// class ATM : login- password*+
+// ctor (id, money)+
+// ShowBalance()+
+// AddMoney()+
+// Withdraw()+
+// Main() : menu
